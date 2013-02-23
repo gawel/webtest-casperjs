6aa03222c530165e040b1c59707cd62178456f10
Gael Pasgrimaud <gael@gawel.org>
2013-02-23 16:15:58 +0100

    improve meta

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1'
 
 setup(name='webtest-casperjs',
       version=version,
       description="Use casperjs with WebTest",
-      long_description="""\
-""",
-      classifiers=[],
-      keywords='wsgi test webtest',
+      long_description=open('README.rst').read(),
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Paste",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+      ],
+      keywords='wsgi test unit tests web casperjs',
       author='Gael Pasgrimaud',
       author_email='gael@gawel.org',
       url='https://github.com/gawel/webtest-casperjs',
